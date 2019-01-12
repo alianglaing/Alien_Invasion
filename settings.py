57cb191fb93b533564e23246682017fc55205f15
alianglaing <1518921741@qq.com>
2019-01-12 11:44:00 +0800

    git commit settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 12 11:15:28 2019
+
+@author: 靓靓
+"""
+
+class Settings():
+    """设置类，存储游戏中所有的设置"""
+    def __init__(self):
+        self.screen_width=1000
+        self.screen_height=600
+        self.bg_color=(230,230,230)
